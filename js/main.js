f7838dd072c325d25f46ecf6f4247954d89cee01
AlexNew2021 <93085938+AlexNew2021@users.noreply.github.com>
2022-06-02 17:12:39 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,3 +7,18 @@ $('#header').vide('./video/video', {
 
 
 });
+
+
+const menu_nav = document.querySelector('#menu_nav');
+const button_mobile_change = document.querySelector('#button_mobile_change');
+const change_pic_button_mobile = document.querySelector('#change_pic_button_mobile');
+
+
+button_mobile_change.onclick = () => {
+    if (menu_nav.classList.toggle('open')) {
+        change_pic_button_mobile.src ="./img/media_img/close.svg"; } 
+        else { change_pic_button_mobile.src ="./img/media_img/open.svg"; }
+    }
+
+
+    AOS.init();
